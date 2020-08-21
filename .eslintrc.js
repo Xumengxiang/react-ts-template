1a1be3e16093854631bca0f0533e2f4651033979
Xumengxiang <1203433230@qq.com>
2020-08-21 17:37:29 +0800

    feat: quote conflict

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,51 +9,56 @@ module.exports = {
     node: true,
   },
   extends: [
-    "airbnb",
-    "airbnb/hooks",
-    "plugin:react/recommended",
-    "plugin:unicorn/recommended",
-    "plugin:promise/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "prettier/@typescript-eslint",
-    "prettier/react",
-    "prettier/unicorn",
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:promise/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+    'prettier/unicorn',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 11,
-    sourceType: "module",
+    sourceType: 'module',
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".tsx", ".ts", ".js", ".json"],
+        extensions: ['.tsx', '.ts', '.js', '.json'],
       },
     },
   },
-  plugins: ["react", "unicorn", "promise", "@typescript-eslint"],
+  plugins: ['react', 'unicorn', 'promise', '@typescript-eslint'],
   rules: {
-    "import/extensions": [
+    'import/extensions': [
       ERROR,
-      "ignorePackages",
+      'ignorePackages',
       {
-        ts: "never",
-        tsx: "never",
-        js: "never",
-        json: "never",
+        ts: 'never',
+        tsx: 'never',
+        js: 'never',
+        json: 'never',
       },
     ],
-    "import/no-extraneous-dependencies": [ERROR, { devDependencies: true }],
-    "import/prefer-default-export": OFF,
-    "import/no-unresolved": ERROR,
+    'import/no-extraneous-dependencies': [
+      ERROR,
+      {
+        devDependencies: true,
+      },
+    ],
+    'import/prefer-default-export': OFF,
+    'import/no-unresolved': ERROR,
 
-    "unicorn/better-regex": ERROR,
-    "unicorn/prevent-abbreviations": OFF,
-    "unicorn/filename-case": [
+    'unicorn/better-regex': ERROR,
+    'unicorn/prevent-abbreviations': OFF,
+    'unicorn/filename-case': [
       ERROR,
       {
         cases: {
@@ -68,45 +73,53 @@ module.exports = {
         },
       },
     ],
-    "unicorn/no-array-instanceof": WARN,
-    "unicorn/no-for-loop": WARN, // 使用 for of 和 .entries 代替传统的 for 循环
-    "unicorn/prefer-add-event-listener": [
+    'unicorn/no-array-instanceof': WARN,
+    'unicorn/no-for-loop': WARN, // 使用 for of 和 .entries 代替传统的 for 循环
+    'unicorn/prefer-add-event-listener': [
       ERROR,
       {
-        excludedPackages: ["koa", "sax"],
+        excludedPackages: ['koa', 'sax'],
       },
     ],
-    "unicorn/prefer-query-selector": ERROR,
-    "unicorn/no-null": OFF,
+    'unicorn/prefer-query-selector': ERROR,
+    'unicorn/no-null': OFF,
 
-    "@typescript-eslint/no-useless-constructor": ERROR,
-    "@typescript-eslint/no-empty-function": WARN,
-    "@typescript-eslint/no-var-requires": OFF,
-    "@typescript-eslint/explicit-function-return-type": OFF,
-    "@typescript-eslint/explicit-module-boundary-types": OFF,
-    "@typescript-eslint/no-explicit-any": OFF,
+    '@typescript-eslint/no-useless-constructor': ERROR,
+    '@typescript-eslint/no-empty-function': WARN,
+    '@typescript-eslint/no-var-requires': OFF,
+    '@typescript-eslint/explicit-function-return-type': OFF,
+    '@typescript-eslint/explicit-module-boundary-types': OFF,
+    '@typescript-eslint/no-explicit-any': OFF,
 
-    "react/jsx-filename-extension": [
+    'react/jsx-filename-extension': [
       ERROR,
-      { extensions: [".tsx", "ts", ".jsx", "js"] },
+      {
+        extensions: ['.tsx', 'ts', '.jsx', 'js'],
+      },
     ],
-    "react/jsx-indent-props": [ERROR, 2],
-    "react/jsx-indent": [ERROR, 2],
-    "react/jsx-one-expression-per-line": OFF,
-    "react/destructuring-assignment": OFF,
-    "react/state-in-constructor": OFF,
-    "react/jsx-props-no-spreading": OFF,
+    'react/jsx-indent-props': [ERROR, 2],
+    'react/jsx-indent': [ERROR, 2],
+    'react/jsx-one-expression-per-line': OFF,
+    'react/destructuring-assignment': OFF,
+    'react/state-in-constructor': OFF,
+    'react/jsx-props-no-spreading': OFF,
 
-    "lines-between-class-members": [ERROR, "always"],
-    indent: [ERROR, 2, { SwitchCase: 1 }],
-    "linebreak-style": [ERROR, "unix"],
-    // quotes: [ERROR, 'single'],
-    semi: [ERROR, "always"],
-    "no-unused-expressions": WARN,
-    "no-plusplus": OFF,
-    "no-console": OFF,
-    "class-methods-use-this": ERROR,
-    "jsx-quotes": [ERROR, "prefer-single"],
-    "global-require": OFF,
+    'lines-between-class-members': [ERROR, 'always'],
+    indent: [
+      ERROR,
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    'linebreak-style': [ERROR, 'unix'],
+    quotes: [ERROR, 'single'],
+    semi: [ERROR, 'always'],
+    'no-unused-expressions': WARN,
+    'no-plusplus': OFF,
+    'no-console': OFF,
+    'class-methods-use-this': ERROR,
+    'jsx-quotes': [ERROR, 'prefer-single'],
+    'global-require': OFF,
   },
 };
