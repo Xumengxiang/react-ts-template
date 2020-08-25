fcf96e263028820f3289a238c5931d857270431f
Xumengxiang <1203433230@qq.com>
2020-08-25 18:16:30 +0800

    feat: 环境配置

--- scripts/config/webpack.common.js
+++ scripts/config/webpack.common.js
@@ -1,12 +1,12 @@
 const path = require('path');
-const { PROJECT_PATH } = require('../constants');
+const { isDev, PROJECT_PATH } = require('../constants');
 
 module.exports = {
   entry: {
     app: path.resolve(PROJECT_PATH, './src/app.js'),
   },
   output: {
-    filename: 'js/[name].[hash:8].js',
+    filename: `js/[name]${isDev ? '' : '.[hash:8]'}.js`,
     path: path.resolve(PROJECT_PATH, './dist'),
   },
 };
