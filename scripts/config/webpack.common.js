554dac3065cea99d37429b4a48b703a9bd7574c0
Xumengxiang <1203433230@qq.com>
2020-08-26 13:34:28 +0800

    feat: dev-server

--- scripts/config/webpack.common.js
+++ scripts/config/webpack.common.js
@@ -1,4 +1,7 @@
+/* eslint-disable indent */
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve } = require('path');
 const { isDev, PROJECT_PATH } = require('../constants');
 
 module.exports = {
@@ -9,4 +12,27 @@ module.exports = {
     filename: `js/[name]${isDev ? '' : '.[hash:8]'}.js`,
     path: path.resolve(PROJECT_PATH, './dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(PROJECT_PATH, './public/index.html'),
+      filename: 'index.html',
+      cache: false,
+      minify: isDev
+        ? false
+        : {
+            removeAttributeQuotes: true,
+            collapseWhitespace: true,
+            removeComments: true,
+            collapseBooleanAttributes: true,
+            collapseInlineTagWhitespace: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            minifyCSS: true,
+            minifyJS: true,
+            minifyURLs: true,
+            useShortDoctype: true,
+          },
+    }),
+  ],
 };
--- scripts/config/webpack.dev.js
+++ scripts/config/webpack.dev.js
@@ -1,6 +1,16 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const { SERVER_HOST, SERVER_PORT } = require('../constants');
 
 module.exports = merge(common, {
   mode: 'development',
+  devServer: {
+    host: SERVER_HOST,
+    port: SERVER_PORT,
+    stats: 'errors-only',
+    clientLogLevel: 'silent',
+    compress: true,
+    open: true,
+    hot: true,
+  },
 });
